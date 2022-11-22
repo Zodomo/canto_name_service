beb5f2ed19a483a3ce0eea50a9e09daf6aaf2a5b
Zodomo <zodomo@proton.me>
2022-11-22 17:16:15 -0600

    imported OZ ERC721 contract, extensions, and libraries

--- src/CantoNameService.sol
+++ src/CantoNameService.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract CantoNameService {
-  
+import "./ERC721.sol";
+import "openzeppelin-contracts/access/Ownable.sol";
+import "openzeppelin-contracts/security/ReentrancyGuard.sol";
+
+contract CantoNameService is ERC721("Canto Name Service", "CNS"), Ownable, ReentrancyGuard {
+
 }
